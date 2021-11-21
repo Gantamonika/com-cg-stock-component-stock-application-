3607b08244be22ce6ce731cec13be3d5b628498c
Monika <gantamonika1998@gmail.com>
2021-11-21 17:56:48 +0530

    commit

--- src/main/java/com/cg/stock/management/demo/model/Stock.java
+++ src/main/java/com/cg/stock/management/demo/model/Stock.java
@@ -1,23 +1,67 @@
 package com.cg.stock.management.demo.model;
 
-public class Stock {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+
+
+@Entity
+public class Stock implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1199218719281064116L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int stockId;
+	
+	@Column
 	private String stockName;
+	
+	@OneToOne
+	@JoinColumn(name = "investorId")
 	private Investor investor;
+	
+	@OneToOne
+	@JoinColumn(name = "companyId")
 	private Company company;
+	
+	@Column
 	private int quantity;
+	
+	@Column
 	private String type;
+	
+	@Column
 	private double avgPrice;
+	
+	@Column
 	private int localStock;
+	
+	@Column
 	private double profitLoss;
+	
+	@Column
 	private String status;
+	
 	public Stock() {
+		
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
+	
 	public Stock(int stockId, String stockName, Investor investor, Company company, int quantity, String type,
 			double avgPrice, int localStock, double profitLoss, String status) {
+		
 		super();
 		this.stockId = stockId;
 		this.stockName = stockName;
@@ -30,66 +74,87 @@ public class Stock {
 		this.profitLoss = profitLoss;
 		this.status = status;
 	}
+	
 	public int getStockId() {
 		return stockId;
 	}
+	
 	public void setStockId(int stockId) {
 		this.stockId = stockId;
 	}
+	
 	public String getStockName() {
 		return stockName;
 	}
+	
 	public void setStockName(String stockName) {
 		this.stockName = stockName;
 	}
+	
 	public Investor getInvestor() {
 		return investor;
 	}
+	
 	public void setInvestor(Investor investor) {
 		this.investor = investor;
 	}
+	
 	public Company getCompany() {
 		return company;
 	}
+	
 	public void setCompany(Company company) {
 		this.company = company;
 	}
+	
 	public int getQuantity() {
 		return quantity;
 	}
+	
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+	
 	public String getType() {
 		return type;
 	}
+	
 	public void setType(String type) {
 		this.type = type;
 	}
+	
 	public double getAvgPrice() {
 		return avgPrice;
 	}
+	
 	public void setAvgPrice(double avgPrice) {
 		this.avgPrice = avgPrice;
 	}
+	
 	public int getLocalStock() {
 		return localStock;
 	}
+	
 	public void setLocalStock(int localStock) {
 		this.localStock = localStock;
 	}
+	
 	public double getProfitLoss() {
 		return profitLoss;
 	}
+	
 	public void setProfitLoss(double profitLoss) {
 		this.profitLoss = profitLoss;
 	}
+	
 	public String getStatus() {
 		return status;
 	}
+	
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	
 	@Override
 	public String toString() {
 		return "Stock [stockId=" + stockId + ", stockName=" + stockName + ", investor=" + investor + ", company="
